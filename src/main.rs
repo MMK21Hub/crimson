1bdb05ee5b16ee88e80e565a4e194af87e9c97e3
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-02-14 06:55:45 +0000

    Better printing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,62 @@
 use anyhow::{Context, Result};
+use clap::{Args, Parser, Subcommand};
 use postgres::{Client, NoTls};
-use time::macros::datetime;
+use time::macros::{datetime, format_description};
 use time::{OffsetDateTime, PrimitiveDateTime};
 
+#[derive(Parser)]
+struct CrimsonArgs {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Payout(PayoutArgs),
+}
+
+#[derive(Args)]
+struct PayoutArgs {
+    /// Start time (ISO 6801, e.g. 2026-02-01T00:00:00Z)
+    #[arg(long)]
+    start: String,
+
+    /// End time (ISO 6801, e.g. 2026-03-01T00:00:00Z)
+    #[arg(long)]
+    end: String,
+}
+
+fn parse_datetime(s: &str) -> Result<OffsetDateTime> {
+    let datetime =
+        OffsetDateTime::parse(s, &time::format_description::well_known::Iso8601::DEFAULT)
+            .context("Invalid datetime string")?;
+    Ok(datetime)
+}
+
 fn main() -> anyhow::Result<()> {
-    // Env var config
+    // Configuration
     dotenvy::dotenv().ok();
     let db_url =
         std::env::var("DATABASE_URL").context("DATABASE_URL environment variable not set")?;
+    let args = CrimsonArgs::parse();
+    let command_args: &PayoutArgs = match &args.command {
+        Command::Payout(p) => p,
+    };
+    let start = parse_datetime(&command_args.start)?;
+    let end = parse_datetime(&command_args.end)?;
+    let pretty_printer = format_description!(
+        "[weekday] [day padding:none] [month repr:short] [year] (@ [hour]:[minute])"
+    );
+    println!(
+        "Selecting leaderboard from {} to {} (Period: {})",
+        start.format(&pretty_printer)?,
+        end.format(&pretty_printer)?,
+        end - start
+    );
 
     let client = Client::connect(&db_url, NoTls)?;
 
-    let x = get_helper_leaderboard(
-        client,
-        datetime!(2026-02-02 20:00:00 UTC),
-        datetime!(2026-02-06 00:00:00 UTC),
-    )?;
+    let x = get_helper_leaderboard(client, start, end)?;
 
     println!("{:#?}", x);
 
